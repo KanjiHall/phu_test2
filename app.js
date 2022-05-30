2bebbe7ef2b5be1ab8eac065d3114aed8f685fd3
Kanji Hall <hongocthienphu17@gmail.com>
2022-05-30 16:23:18 +0700

    Heroku

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// router.get('/test', function (req, res) {
+//     const test = { "NetW0rk":"4R3nS1c"};
+//     res.status(200).json(test);
+// });
 router.get('/test', function (req, res) {
-    const test = { "NetW0rk":"4R3nS1c"};
-    res.status(200).json(test);
+    res.send("srcflag:{abc}");
 });
 
 app.use('/', router);
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
     console.log("Server is running.");
-  });
+});
